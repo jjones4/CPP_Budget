d0f269be85befb4530bee5805ffc74e098cc890f
Alma <alma@alma-c-box.local>
2022-10-26 21:21:21 -0500

    Add Tests for the Update Menu Input Validation Functions

---
+++ tests/Validation/update_menu_input.cpp
@@ -0,0 +1,128 @@
+/*
+ * Name:       update_menu_input.cpp
+ *
+ * Purpose:    Contains functions for testing the different
+ *             steps in validating update menu input.
+ *
+ *             1. Check if the input is an integer
+ *             2. Check if the input is within the acceptable
+ *                range for the  update menu's input
+ *             3. Display the test results of tests from
+ *                an infile called update_menu_input_infile
+ *
+ * Note:       I have included an "infile" with some test data.
+ *             Run the following command with the infile
+ *             that is provided for testing:
+ *             
+ *                update_menu_input < update_menu_input_infile
+ *
+ * Author:     jjones4
+ *
+ * Copyright (c) 2022 Jerad Jones
+ * This file is part of CPP_Budget.  CPP_Budget may be freely
+ * distributed under the MIT license.  For all details and
+ * documentation, see https://github.com/jjones4/CPP_Budget
+ *
+ */
+
+#include <iostream>
+#include <string>
+
+#define UPDATE_MENU_INPUT_MIN 1
+#define UPDATE_MENU_INPUT_MAX 5
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::stoi;
+
+void display_results_of_test(string update_menu_input);
+bool is_non_negative_integer(const string s);
+bool is_within_update_menu_range(const string n);
+
+int main()
+{
+   string update_menu_input;
+
+   // If there is data to process
+   if(getline(cin, update_menu_input))
+   {
+      display_results_of_test(update_menu_input);
+
+      // If there is data to process, get the rest of it
+      while(getline(cin, update_menu_input))
+      {
+         display_results_of_test(update_menu_input);
+      }
+   }
+   else
+   {
+      cout << "No data to process." << endl;
+      return -1;
+   }
+
+   return 0;
+}
+
+void display_results_of_test(string update_menu_input)
+{
+   if(!is_non_negative_integer(update_menu_input))
+   {
+      cout << "Not a positive integer or zero --> "
+           << update_menu_input
+           << endl;
+      return;
+   }
+
+   if(!is_within_update_menu_range(update_menu_input))
+   {
+      cout << "Not within range               --> "
+           << update_menu_input
+           << endl;
+      return;
+   }
+ 
+   if(is_non_negative_integer(update_menu_input) && is_within_update_menu_range(update_menu_input))
+   {
+      cout << "Test passes! Valid input       --> "
+           << update_menu_input
+           << endl;
+      return;
+   }
+}
+
+// Will be shared by both functions that validate menu input
+// 1) is_valid_main_menu_option() - for the main menu
+// 2) is_valid_update_menu_option() - for when the user is chosing
+//    which part of the budget transaction they want to update
+bool is_non_negative_integer(const string s)
+{
+   // Empty line is integer; handle this specific case
+   if(s == "")
+   {
+      return false;
+   }
+
+   for(unsigned int i = 0; i < s.length(); i++)
+   {
+      if(isdigit(s[i]) == false)
+      {
+         return false;
+      }
+   }
+
+   return true;
+}
+
+bool is_within_update_menu_range(const string s)
+{
+   if(stoi(s) >= UPDATE_MENU_INPUT_MIN && stoi(s) <= UPDATE_MENU_INPUT_MAX)
+   {
+      return true;
+   }
+   else
+   {
+      return false;
+   }
+}
